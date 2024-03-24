db783bbf67f64a63d2357fef734364647d0234e8
PreyMa <mlpreymann@gmail.com>
2024-03-24 16:47:57 +0100

    JS: Use the state stack of context2D

--- JS/public/renderer.js
+++ JS/public/renderer.js
@@ -1,37 +1,20 @@
 
-class RenderState {
-  constructor() {
-    this.fillColor= 'white'
-    this.strokeColor= 'black'
-    this.strokeWeight= 1
-    this.doFill= true
-    this.doStroke= true
-  }
-
-  applyAll( context ) {
-    context.fillStyle = this.fillColor
-    context.strokeStyle = this.strokeColor
-    context.lineWidth = this.strokeWeight
-  }
-
-  applyFillColor( context ) {
-    context.fillStyle = this.fillColor
-  }
-}
-
+/** The Renderer draws the game map elements on the canvas */
 export class Renderer {
+  #canvas
+  #context
+
+  /** @param {HTMLCanvasElement} canvas */
   constructor( canvas ) {
-    this.canvas= canvas
-    this.context= this.canvas.getContext('2d')
-    this.states= [ new RenderState() ]
-
-    this._currentState.applyAll( this.context );
-
-    this.createFilter( 'remove-alpha' )
-    this.context.filter= 'url(#remove-alpha)'
+    this.#canvas= canvas
+    this.#context= this.#canvas.getContext('2d')
+    
+    this.#createFilter( 'remove-alpha' )
+    this.#context.filter= 'url(#remove-alpha)'
   }
 
-  createFilter( filterId ) {
+  /** Creates a filter to disable aliasing, ensuring a pixelated look of objects */
+  #createFilter( filterId ) {
     function createSVGElement( tagName, attributes ) {
       const svgNamespace= 'http://www.w3.org/2000/svg'
       const element= document.createElementNS( svgNamespace, tagName )
@@ -48,55 +31,60 @@ export class Renderer {
       .appendChild( createSVGElement('filter', {id: filterId, x: 0, y: 0, width: '100%', height: '100%'}) )
       .appendChild( createSVGElement('feComponentTransfer') )
       .appendChild( createSVGElement('feFuncA', {type: 'discrete', tableValues: '0 1'}) )
-   }
-  
-  get _currentState() {
-    return this.states[ this.states.length -1 ]
+  }
+
+  #completePath() {
+    if( this.#context.fillStyle !== '#0000' ) {
+      this.#context.fill()
+    }
+
+    if( this.#context.strokeStyle !== '#0000' ) {
+      this.#context.stroke()
+    }
+  }
+
+  /** Saves current set of drawing options */
+  pushState() {
+    this.#context.save()
+  }
+
+  /** Return to previously saved set drawing options */
+  popState() {
+    this.#context.restore()
   }
   
   get height() {
-    return this.canvas.height
+    return this.#canvas.height
   }
   
   get width() {
-    return this.canvas.width
+    return this.#canvas.width
   }
   
   // TODO: Implement setting doFill/doStroke to false 
   set fillColor( color ) {
-    this._currentState.doFill = true
-    this._currentState.fillColor = color
-    this.context.fillStyle = color
+    this.#context.fillStyle = color
   }
 
   set strokeColor( color ) {
-    this._currentState.doStroke = true
-    this._currentState.strokeColor = color
-    this.context.strokeStyle = color
+    this.#context.strokeStyle = color
   }
 
   set strokeWeight( width ) {
-    this._currentState.doStroke = true
-    this._currentState.strokeWeight = width
-    this.context.lineWidth = width
+      this.#context.lineWidth = width
   }
   
   background( color ) {
-    this.context.fillStyle= color
-    this.context.fillRect(0, 0, this.width, this.height)
-    this._currentState.applyFillColor( this.context )
+    const oldFillStyle= this.#context.fillStyle
+    this.#context.fillStyle= color
+    this.#context.fillRect(0, 0, this.width, this.height)
+    this.#context.fillStyle= oldFillStyle
   }
 
   drawCircle(centerX, centerY, radius ) {
-    this.context.beginPath()
-    this.context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false)
-    
-    if( this._currentState.doFill ) {
-      this.context.fill()
-    }
-    
-    if( this._currentState.doStroke ) {
-      this.context.stroke()
-    }
+    this.#context.beginPath()
+    this.#context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false)
+    this.#completePath()
+  }
   }
 }
